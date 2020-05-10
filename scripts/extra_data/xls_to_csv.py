e62f7999858cc620c453e104fdd05c87bc58a52b
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-05-10 17:37:55 +0200

    Custom waste spreading by category and activity (part 3)

--- scripts/extra_data/xls_to_csv.py
+++ scripts/extra_data/xls_to_csv.py
@@ -23,20 +23,35 @@ from autopaths import Path
 import pandas
 
 # Internal modules #
+from waste_flow import module_dir
+
+# Constants #
+convert_pairs = (('extra_data_xls/nace_to_full_name.xlsx',  'extra_data_csv/'),
+                 ('extra_data_xls/waste_to_full_name.xlsx', 'extra_data_csv/'))
 
 ###############################################################################
 class ConvertExcelToCSV:
     """
-    Lorem ipsum
+    Will convert an excel file into its CSV equivalent.
     """
 
-    def __init__(self, path):
-        self.path = Path(path)
+    def __init__(self, source, dest, **kwargs):
+        # Record attributes #
+        self.source = Path(source)
+        self.dest   = Path(dest)
+        self.kwargs = kwargs
+        # Check directory case #
+        if self.dest.endswith('/'):
+            self.dest = self.dest + self.source.filename
+            self.dest = self.dest.replace_extension('csv')
 
     def __call__(self):
-        pass
+        data_xls = pandas.read_excel(str(self.source))
+        data_xls.to_csv(str(self.dest), **self.kwargs)
 
 ###############################################################################
 if __name__ == '__main__':
-    converter = ConvertExcelToCSV('~/test/waste_spreading.xlsx')
-    converter()
+    for pair in convert_pairs:
+        source, dest = module_dir + pair[0], module_dir + pair[1]
+        converter = ConvertExcelToCSV(source, dest, index=False)
+        converter()
