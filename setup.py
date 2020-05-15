0594a4aa8ca1b23ef1151b9f0e0b1819cfcbb451
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-05-15 18:32:41 +0200

    Version 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name             = 'waste_flow',
-        version          = '1.1.0',
+        version          = '1.1.1',
         description      = 'A package for retrieving data concerning waste management on the European continent.',
         long_description = open('README.md').read(),
         long_description_content_type = 'text/markdown',
