0bf287fc5f43135d989a5beb168712ea9133b120
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-07-09 00:18:38 +0200

    Better setup script organization

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
         name             = 'waste_flow',
         version          = '1.2.0',
         description      = 'A package for retrieving data concerning waste management on the European continent.',
-        long_description = open('README.md').read(),
-        long_description_content_type = 'text/markdown',
         license          = 'MIT',
         url              = 'http://github.com/xapple/waste_flow/',
         author           = 'Lucas Sinclair',
         author_email     = 'lucas.sinclair@me.com',
         packages         = find_packages(),
-        install_requires = ['pandas>=1.0.0', 'numpy>=1.16', 'requests', 'matplotlib',
-                            'autopaths>=1.4.0', 'plumbing>=2.7.9', 'pymarktex>=1.3.8'],
+        install_requires = ['autopaths>=1.4.0', 'plumbing>=2.7.9', 'pymarktex>=1.3.8'
+                            'pandas>=1.0.0', 'numpy>=1.16', 'requests', 'matplotlib'],
+        long_description = open('README.md').read(),
+        long_description_content_type = 'text/markdown',
         include_package_data = True,
 )
