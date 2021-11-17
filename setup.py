eed5d73dfad722e97c28875a05c0003b6a4489e0
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-11-17 21:13:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
-from setuptools import setup, find_namespace_packages
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+# Imports #
+from setuptools import setup, find_namespace_packages
+from os import path
+
+# Load the contents of the README.md file #
+this_dir = path.abspath(path.dirname(__file__))
+readme_path = path.join(this_dir, 'README.md')
+with open(readme_path, encoding='utf-8') as handle: readme = handle.read()
+
+# Call setup #
 setup(
-        name             = 'waste_flow',
-        version          = '1.2.0',
-        description      = 'A package for retrieving data concerning waste management on the European continent.',
-        license          = 'MIT',
-        url              = 'http://github.com/xapple/waste_flow/',
-        author           = 'Lucas Sinclair',
-        author_email     = 'lucas.sinclair@me.com',
-        packages         = find_namespace_packages(),
-        install_requires = ['autopaths>=1.4.0', 'plumbing>=2.7.9', 'pymarktex>=1.3.8'
-                            'pandas>=1.0.0', 'numpy>=1.16', 'requests', 'matplotlib'],
-        long_description = open('README.md').read(),
-        long_description_content_type = 'text/markdown',
-        include_package_data = True,
+    name             = 'waste_flow',
+    version          = '1.3.0',
+    description      = 'A package for retrieving data concerning waste '
+                       'management on the European continent.',
+    license          = 'MIT',
+    url              = 'http://github.com/xapple/waste_flow/',
+    author           = 'Lucas Sinclair',
+    author_email     = 'lucas.sinclair@me.com',
+    packages         = find_namespace_packages(),
+        install_requires = ['autopaths>=1.5.9', 'plumbing>=2.11.1',
+                            'pymarktex>=1.5.7' 'pandas', 'numpy',
+                            'pytest', 'requests', 'matplotlib'],
+    python_requires  = ">=3.6",
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+    include_package_data = True,
 )
